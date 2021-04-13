225837cb196e06e4b19a6d3054a831ec2adb8f6d
LoneHippie <lonehippie.music@gmail.com>
2021-04-14 01:33:13 +0300

    ugh

--- app.js
+++ app.js
@@ -44,7 +44,22 @@ app.options("/*", function(req, res, next){
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
     res.send(200);
-  });
+});
+
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
 
 //serving static files
 //lets static files be accessed through images/ of uploads/ in frontend
