98676dd4ce6c8983f4e0c0cfea9c58bd7e40c3bf
LoneHippie <lonehippie.music@gmail.com>
2021-04-13 20:59:22 +0300

    yyyYYYYYASYDYASD

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/images', express.static('images'));
 app.use(helmet());
 
 const corsOptions = {
-    origin: 'https://drinkdex.netlify.app/',
+    origin: 'https://drinkdex.netlify.app',
     credentials:  true
 };
 
