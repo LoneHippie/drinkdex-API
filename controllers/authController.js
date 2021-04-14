9f955d9ea1ad2567e559fec3b38a6ce206e4c9c6
LoneHippie <lonehippie.music@gmail.com>
2021-04-14 11:49:45 +0300

    domain is correct?

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,9 +17,8 @@ const createSendToken = (user, statusCode, req, res) => {
     const token = signToken(user._id);
 
     const cookieOptions = {
-        domain: 'https://drinkdex.netlify.app/',
-        // secure: req.secure || req.headers('x-forwarded-proto') === 'https', //heroku specific line
-        secure: true,
+        domain: 'https://drinkdex.netlify.app',
+        secure: req.secure || req.headers('x-forwarded-proto') === 'https', //heroku specific line
         httpOnly: true,
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000)
     };
